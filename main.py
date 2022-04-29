b3b5eebf7ab15fb45b19cf9e5063e428de3ab4ce
Ethan <65051086+waitblock@users.noreply.github.com>
2022-04-28 21:38:53 -0400

    feat: the bot will now translate msgs :smiley:

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import discord
 from discord.ext import commands
+from translate import Translator
+
+
+# Configuration variables:
+BOT_ID = 969008594097930331  # The ID of your translate bot
+TRANSLATE_LANGUAGE_CODE = "zh"  # This is the ISO 639-1 two letter language code that the bot will translate to
+
 
 # Print bot information
 print("Discord Translate v0.0.1")
@@ -7,7 +14,21 @@ print("(c) 2022 Ethan (waitblock) under the MIT License")
 print("This bot is NOT in affiliation with Discord Inc.")
 print("="*60)
 
+
+# Parallel arrays containing the ISO 639-1 two letter language codes, and the language of the two letter language codes in English
+language_codes = []
+language_names = []
+
+# Read in language codes
+with open("language_codes.csv", "r") as language_codes_file:
+    language_codes_csv = language_codes_file.read().split("\n")
+    for language_code_csv in language_codes_csv:
+        language_codes.append(language_code_csv.split(",")[0])
+        language_names.append(language_code_csv.split(",")[1])
+
+
 translate_channels = []  # Channels IDs that will have translations enabled.
+
 # Read & parse channels from channels.txt file
 try:
     with open("channels.txt", "r") as channels_file:
@@ -17,12 +38,34 @@ try:
 except FileNotFoundError:  # channels.txt file does not exist
     print("Please create a file with the name 'channels.txt' to configure the bot.")
     exit()
+except ValueError:  # channels.txt contains a channel ID that is not an integer
+    print("Please remove any channel IDs in 'channels.txt' that are not valid channel IDs / integers.")
+    exit()
 
+
+# Print out the channels that will be translated
 print("Channel(s) that will be translated (The channel ID(s) are shown):")
 for channel in translate_channels:
     print(channel)
 
-bot = commands.Bot(("translate!", "tr!"), case_insensitive=True, help_command=None)
+if TRANSLATE_LANGUAGE_CODE not in language_codes:
+    print("The given language code in the configuration variable 'TRANSLATE_LANGUAGE' is an invalid ISO 639-1 two-letter language code.")
+    print("Please enter a valid ISO 639-1 two-letter language code and try again.")
+    exit()
+
+TRANSLATE_LANGUAGE_NAME = language_names[language_codes.index(TRANSLATE_LANGUAGE_CODE)]  # Name of language being translated to
+print(f"Configured to translate from English to {TRANSLATE_LANGUAGE_NAME}.")
+
+
+translator = Translator(to_lang=TRANSLATE_LANGUAGE_CODE)
+
+
+# Enable the 'messages' intent
+intents = discord.Intents.default()
+intents.messages = True
+
+
+bot = commands.Bot(("translate!", "tr!"), case_insensitive=True, help_command=None, intents=intents)
 
 
 @bot.command(name="ping")
@@ -31,9 +74,16 @@ async def ping(ctx):
 
 
 @bot.event
-async def on_message(message):
-    if message.channel.id in translate_channels:
-        print(message.content)
+async def on_message(ctx):
+    if ctx.author.id == BOT_ID:  # Ignore the bot's own messages
+        return
+    if ctx.channel.id in translate_channels:
+        original_message = ctx.content
+        translated_message = translator.translate(original_message)
+        output = f"{original_message} (English) -> ({TRANSLATE_LANGUAGE_NAME}) {translated_message}"
+        await ctx.reply(output, mention_author=False)
+        print(output)
+    await bot.process_commands(ctx)
 
 
 @bot.event
@@ -41,6 +91,7 @@ async def on_connect():
     print("Bot connected to Discord API")
     print("="*60)
     await bot.change_presence(activity=discord.Game(name="For help type translate!help"))
+
 
 with open("TOKEN", "r") as token:
     bot.run(token.read())
